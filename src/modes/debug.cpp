1c3baeb812bdd508ad74326aca5f8e2cb2d0a023
Alexey <87594443+alekami649@users.noreply.github.com>
2023-02-11 10:56:13 +0000

    debug

--- src/modes/debug.cpp
+++ src/modes/debug.cpp
@@ -1,5 +1,6 @@
 #include "debug.hpp"
 #include "nlohmann/json.hpp"
+#include <filesystem>
 #include <fstream>
 #include <string>
 
@@ -8,9 +9,11 @@ using namespace nlohmann;
 void Build(string project, bool allFolder) {
   ifstream pstream;
   pstream.open(project);
+  string functions[123];
   if (pstream.is_open()) {
     json pjson = json::parse(pstream.rdbuf());
     pjson[""];
   }
+  GetModuleFileName
 }
 bool Test(string project, bool allFolder) { return true; }
